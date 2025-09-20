2e186384dfbfec008851799c0a547f614bc22148
Martin Majsec <martin.majsec@fer.hr>
2025-09-20 12:15:02 +0200

    add linear regression
    The first two functions in Step 2

--- assignment/implementations.py
+++ assignment/implementations.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def compute_loss(y, tx, w):
-    """Calculate the loss using either MSE or MAE.
+    """Calculate the loss using MSE.
 
     Args:
         y: numpy array of shape=(N, )
@@ -15,6 +15,21 @@ def compute_loss(y, tx, w):
     e = y - tx @ w
     average = np.mean(e ** 2) / 2
     return average
+
+def compute_gradient(y, tx, w):
+    """Computes the gradient at w.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
+    """
+    N = len(y)
+    e = y - (tx @ w)
+    return -1 / N * np.transpose(tx) @ e 
 
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
@@ -32,9 +47,35 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     """
     w = initial_w.copy()
     for _ in range(max_iters):
-        y_pred = tx @ w
-        error = y_pred - y
-        grad = tx.T @ error / len(y)
-        w -= gamma * grad
+        # y_pred = tx @ w
+        # error = y_pred - y
+        # grad = tx.T @ error / len(y)
+        grad = compute_gradient(y, tx, w)
+        w = w - gamma * grad
     loss = compute_loss(y, tx, w)
     return w, loss
+
+def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
+    """Linear regression using stochastic gradient descent.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        initial_w: numpy array of shape=(2,). The vector of model parameters.
+        max_iters: int. The number of iterations to run the algorithm.
+        gamma: float. The stepsize (learning rate).
+
+    Returns:
+        (w, loss): tuple of numpy array of shape (2,) w last weight and float.
+    """
+    w = initial_w.copy()
+    N = len(y)
+    for _ in range(max_iters):
+        sample_datapoint = np.random.randint(N)
+        grad = compute_gradient(y[sample_datapoint], tx[sample_datapoint], w)
+        w = w - gamma * grad
+    loss = compute_loss(y, tx, w)
+    return w, loss
+        
+        
+
