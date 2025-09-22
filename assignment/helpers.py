24ed22136fff441e8e253dd8837777d399e8a201
Martin Majsec <martin.majsec@fer.hr>
2025-09-22 12:26:43 +0200

    add logistic error. Find error in least squares
    OLS: The least squares method seems to have a problem with singular matrices. Refer to the code for more detail.
    
    SGD: When the number of considered datapoints increases to a reasonable size, the SGD performs terribly. I guess this is expected because we have to implement it with batch-size 1, so we'll leave the implementation as-is. The implementation still needs to be tested just in case. If we ever want to actually use SGD, we'll have to increase the batch-size for sure.

--- assignment/helpers.py
+++ assignment/helpers.py
@@ -43,6 +43,8 @@ def load_csv_data(data_path, max_rows = None, max_features = None, NaNstrat = No
     x_train = x_train[:, 1:]
     x_test = x_test[:, 1:]
 
+    # remove duplicate rows
+
     # sub-sample
     if sub_sample: # unused
         y_train = y_train[::50]
@@ -59,10 +61,13 @@ def load_csv_data(data_path, max_rows = None, max_features = None, NaNstrat = No
         train_ids = train_ids[:max_rows]
     
     if NaNstrat:
+        # remove all columns that contain only NaNs
+        NaNcols = ~np.all(np.isnan(x_train), axis=0)
+        x_train = x_train[:, NaNcols]
         # temporary solution: fill NaNs with column mean
         col_means = np.nanmean(x_train, axis=0)
-        inds = np.where(np.isnan(x_train))
-        x_train[inds] = np.take(col_means, inds[1])
+        NaNrows = np.where(np.isnan(x_train))
+        x_train[NaNrows] = np.take(col_means, NaNrows[1])
 
 
     return x_train, x_test, y_train, train_ids, test_ids
@@ -89,3 +94,20 @@ def create_csv_submission(ids, y_pred, name):
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
             writer.writerow({"Id": int(r1), "Prediction": int(r2)})
+
+def print_result(method_name, loss, w, additional_info = None):
+    """
+    This function formats and prints the outputs of a regression method.
+    Loss is rounded to 5 decimals, and weights to 2.
+    If `additional_info` is provided, it will be printed at the end of the output.
+
+    Args:
+        method_name (str): name of the method used
+        loss (float): loss returned by the regression method
+        w (list, np.array): weights returned by the regression method
+        additional_info (str): optional information that could be printed at the end of the output
+    """
+    out = f"[{method_name}] loss = {loss:.5f}, w = {np.round(w, 2)}"
+    if additional_info:
+        out += f" ({additional_info})"
+    print(out)
--- assignment/implementations.py
+++ assignment/implementations.py
@@ -27,8 +27,6 @@ def compute_gradient(y, tx, w):
     Returns:
         An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
     """
-    # print(f"compute_gradient got y = {y}, tx = {tx}, w = {w}")
-
     N = tx.shape[0]
     y_pred = tx @ w
     error = y_pred - y
@@ -70,7 +68,7 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
         (w, loss): tuple of numpy array of shape (2,) w last weight and float.
     """
     w = initial_w.copy()
-    N = tx.shape[1]
+    N = len(tx)
     for _ in range(max_iters):
         random_index = np.random.randint(N)
         sampled_y = np.array([y[random_index]])
@@ -80,7 +78,7 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     loss = compute_loss(y, tx, w)
     return w, loss
         
-        
+
 
 def least_squares(y, tx):
     """Least squares regression using normal equations.
@@ -93,12 +91,20 @@ def least_squares(y, tx):
         (w, loss): tuple of numpy array of shape (2,) w last weight and float.
     """
     # Proceed carefully: naive impletentaion is ill-conditioned.  
-    # Let np.linalg do the hard work. -M
-
-    # solve Mw = b
+    # Let np.linalg.solve do the hard work. 
+    # Note: tx.T @ tx seems to be singular, which is a problem for np.linalg.solve.
+    # There are multiple ways to combat this: 
+    # - removing linearly dependent rows and columns
+    # - adding regularization; but that would be equivalent to ridge/lasso
+    # I suggest we try fixing it in the 1st way, but will check with the TA's as well. -M
+ 
     M = tx.T @ tx
     b = tx.T @ y
-    w = np.linalg.solve(M, b)  
+    try:
+        w = np.linalg.solve(M, b)  
+    except:
+        print("Oh no, the matrix M is singular! M =", len(M))
+        a = input()
     loss = compute_loss(y, tx, w)
     return w, loss
 
@@ -121,3 +127,97 @@ def ridge_regression(y, tx, lambda_):
     loss = compute_loss(y, tx, w)
     return w, loss
 
+
+def sigmoid(x):
+    """Activation function for logistic regression.
+    Args:
+        x: numpy array of shape=(N, )
+    Returns:
+        sigmoid(x): numpy array of shape=(N, )
+    """
+    return 1 / (1 + np.exp(-x))
+
+def compute_logistic_loss(y, tx, w):
+    """Calculate the loss using sigmoid activation function.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    y = (y == 1).astype(int) # convert y from {-1, +1} to {0, 1}
+    z = tx @ w
+    epsilon = 1e-5 # epsilon trick to control values close to 0 and 1. Source: https://stackoverflow.com/questions/38125319/python-divide-by-zero-encountered-in-log-logistic-regression
+    term1 = y * np.log(sigmoid(z) + epsilon)
+    term2 = (1 - y) * np.log(1 - sigmoid(z) + epsilon)
+    return -np.mean(term1 + term2)
+
+def compute_logistic_gradient(y, tx, w):
+    """Computes the gradient of the logistic loss function at w.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
+    """
+    N = tx.shape[0]
+    e = sigmoid(tx @ w) - y
+    average = (tx.T @ e) / N
+    return average
+
+def logistic_error_gd(y, tx, initial_w, max_iters, gamma):
+    """Logistic regression using gradient descent.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        initial_w: numpy array of shape=(2,). The vector of model parameters.
+        max_iters: int. The number of iterations to run the algorithm.
+        gamma: float. The stepsize (learning rate).
+
+    Returns:
+        (w, loss): tuple of numpy array of shape (2,) w last weight and float.
+    """
+    w = initial_w.copy()
+    for _ in range(max_iters):
+        grad = compute_logistic_gradient(y, tx, w)
+        w = w - gamma * grad
+    loss = compute_logistic_loss(y, tx, w)
+    return w, loss
+
+
+def preprocess(x_train):
+    """
+    Preprocess training data (no test data yet).
+    In particular this means:
+        - remove 0-variance columns, they don't provide any useful information
+        - standardize each column (feature)
+        - clip extreme outliers after standardizations
+        - prepend a bias column of 1s
+
+    Args:
+        x_train: numpy array of shape (N,D)
+
+    Returns:
+        tx: numpy array of shape (N,D'), where D' <= D + 1
+    """
+    # Remove 0-variance columns
+    mask = x_train.std(axis=0) != 0
+    x_train = x_train[:, mask]
+    # Standardize
+    x_mean = np.nanmean(x_train, axis=0)
+    x_std = np.nanstd(x_train, axis=0)
+    x_train = (x_train - x_mean) / x_std
+    # Clip extreme outliers
+    x_train = np.clip(x_train, -5, 5)
+    # Prepend the 1s column 
+    ones = np.ones((len(x_train),1), dtype=float)
+    tx = np.hstack((ones, x_train.astype(float)))
+    # Done!
+    return tx
