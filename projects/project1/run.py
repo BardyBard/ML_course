cb1dd4d33d27e96ed2034f5315a0b16d04c70fb9
Martin Majsec <martin.majsec@fer.hr>
2025-10-31 15:33:13 +0100

    (chore) commit code changes

--- projects/project1/run.py
+++ projects/project1/run.py
@@ -13,7 +13,7 @@ if model_to_run == "ID3":
     path_to_dataset = "data/dataset"
     x_train, x_test, y_train, train_ids, test_ids = load_csv_data(
         path_to_dataset, NaNstrat="fill"
-    )  # loading is too slow, I use max_rows and max_features for testing -M
+    )  
 
     # set hyperparams
     k = 3  # bin size
